cc743b83a4c0c3b1578e141bf04dc7854a40e408
Divya <divyabhatt16@gmail.com>
2022-03-16 07:03:07 +0000

    implemented intialzation method for variables required in the program based on input from TDD

--- src/mars_main.js
+++ src/mars_main.js
@@ -2,7 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RoversOnMars = void 0;
 class RoversOnMars {
+    constructor() {
+        this.plateau = [];
+        this.rovers = [];
+    }
+    intialize(input) {
+        let index = 0;
+        for (const [key, value] of Object.entries(input)) {
+            if (Number.parseInt(key) === 0) {
+                this.plateau = value;
+            }
+            else {
+                this.rovers[index] = { x: value[0], y: value[1], orientation: value[2], directionToMove: value[3] };
+                index += 1;
+            }
+        }
+        console.log("_plateau=" + this.plateau);
+        console.log("_rovers=" + this.rovers[0]);
+    }
     roversPositionMovedTo(input) {
+        this.intialize(input);
         return [[1, 3, "N"], [5, 1, "E"]];
     }
 }
--- src/mars_main.ts
+++ src/mars_main.ts
@@ -2,7 +2,27 @@ import { Rover } from "./rover";
 
 export class RoversOnMars {
 
+    plateau: Array<number> = []
+    rovers: Array<Rover> = []
+
+    intialize(input: Object): void {
+        let index: number = 0
+        for (const [key, value] of Object.entries(input)) {
+            if (Number.parseInt(key) === 0) {
+                this.plateau = value
+            } else {
+                this.rovers[index] = { x: value[0], y: value[1], orientation: value[2], directionToMove: value[3] }
+                index += 1
+            }
+        }
+
+        console.log("_plateau=" + this.plateau);
+        console.log("_rovers=" + this.rovers[0]);
+
+    }
+
     roversPositionMovedTo(input: Object): Object {
+        this.intialize(input)
         return [[1, 3, "N"], [5, 1, "E"]] as Object
     }
 
