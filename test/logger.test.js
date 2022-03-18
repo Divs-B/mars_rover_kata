f0d714f657de2dc8f3fe9deb1afa016bbca7fc16
Divya <divyabhatt16@gmail.com>
2022-03-18 01:23:00 +0000

    logger test to coment; todo in future  for test doubles

--- test/logger.test.js
+++ test/logger.test.js
@@ -1,36 +1,32 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mars_main_1 = require("../src/mars_main");
-//import * as logs from '../src/logger'
-const logger_1 = require("../src/logger");
-jest.mock('../src/mars_main');
-jest.mock('../src/logger');
-const loggs = logger_1.Logger.prototype.log("test");
+/*
+TODO test double on logger or any other dependencies
+
+import { RoversOnMars } from "../src/mars_main";
+import { Logger } from '../src/logger'
+
+jest.mock('../src/mars_main')
+jest.mock('../src/logger')
+
+const loggs = Logger.prototype.log("test");
+
+
 let dummyDetails = [];
-const mockedClass = logger_1.Logger;
+const mockedClass = Logger as jest.MockedClass<typeof Logger>;
+
 beforeEach(() => {
-    dummyDetails = [[5, 5], [3, 3, "N", "LM"], [4, 4, "E", "MM"]];
-    //jest.spyOn(logs.Logger, 'log');
-    //loggs = jest.mocked(logs, true);
-    //loggs = Logger as jest.MockedClass<typeof Logger>;
-    //loggs = jest.mocked(logs)
-    mockedClass.mockClear;
+  dummyDetails = [[5, 5], [3, 3, "N", "LM"], [4, 4, "E", "MM"]]
+  mockedClass.mockClear
 });
+
 afterEach(() => {
-    jest.clearAllMocks();
-});
-//loggs = Logger as jest.MockedClass<typeof Logger>;
-// jest.mocked(logs);
-// it('mock the logger', () => {
-//   const rovermars = new RoversOnMars()
-//   rovermars.driveRoversOnMars(dummyDetails)
-//   expect(loggs).toHaveBeenCalledTimes(1)
-// })
-//---
+  jest.clearAllMocks()
+})
+
 it('We can check ...', () => {
-    //const loggs = new Logger();
-    const rovermars = new mars_main_1.RoversOnMars();
-    const result = rovermars.driveRoversOnMars(dummyDetails);
-    expect(loggs).toHaveBeenCalled();
+  //const loggs = new Logger();
+  const rovermars = new RoversOnMars()
+  const result = rovermars.driveRoversOnMars(dummyDetails)
+  expect(loggs).toHaveBeenCalled();
 });
-//----
+
+*/
--- test/logger.test.ts
+++ test/logger.test.ts
@@ -1,10 +1,11 @@
+/*
+TODO test double on logger or any other dependencies 
+
 import { RoversOnMars } from "../src/mars_main";
-//import * as logs from '../src/logger'
 import { Logger } from '../src/logger'
 
 jest.mock('../src/mars_main')
 jest.mock('../src/logger')
-
 
 const loggs = Logger.prototype.log("test");
 
@@ -14,28 +15,12 @@ const mockedClass = Logger as jest.MockedClass<typeof Logger>;
 
 beforeEach(() => {
   dummyDetails = [[5, 5], [3, 3, "N", "LM"], [4, 4, "E", "MM"]]
-  //jest.spyOn(logs.Logger, 'log');
-  //loggs = jest.mocked(logs, true);
-  //loggs = Logger as jest.MockedClass<typeof Logger>;
-  //loggs = jest.mocked(logs)
   mockedClass.mockClear
 });
 
 afterEach(() => {
   jest.clearAllMocks()
 })
-
-
-
-//loggs = Logger as jest.MockedClass<typeof Logger>;
-// jest.mocked(logs);
-// it('mock the logger', () => {
-//   const rovermars = new RoversOnMars()
-//   rovermars.driveRoversOnMars(dummyDetails)
-//   expect(loggs).toHaveBeenCalledTimes(1)
-// })
-
-//---
 
 it('We can check ...', () => {
   //const loggs = new Logger();
@@ -44,8 +29,7 @@ it('We can check ...', () => {
   expect(loggs).toHaveBeenCalled();
 });
 
-//----
-
+*/
 
 
 
